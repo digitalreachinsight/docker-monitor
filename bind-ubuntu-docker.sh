6bda7265af7dac5be638c65d60a417f5e1e70602
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2021-04-07 11:24:27 +0800

    Updates

--- bind-ubuntu-docker.sh
+++ bind-ubuntu-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version='v1.0.1'
+version='v1.0.4'
 imagename="bind-ubuntu"
 unhealthycount=$(docker ps | grep unhealthy | grep $imagename | wc -l);
 healthy=$(docker ps | grep "(healthy)" | grep $imagename | grep $version | wc -l);
@@ -9,11 +9,11 @@ echo $starting;
 if [ $healthy -eq 0 ]
 then
    if [ $starting -eq 0 ]
-   then 	   
-      echo "Unhealthy - restarting";	
-      docker stop $imagename 
+   then
+      echo "Unhealthy - restarting";
+      docker stop $imagename
       docker rm $imagename
-      docker container run --name=$imagename --mount type=bind,source=/container-data/bind-container/,target=/shared-mount/  --mount type=bind,source=/rotating-backups,target=/backups  -p 5300:53 -p 5300:53/udp -d -i -t  digitalreachinsight/bind-ubuntu:$version
+      docker container run --name=$imagename --mount type=bind,source=/container-data/bind-container/,target=/shared-mount/  --mount type=bind,source=/rotating-backups,target=/backups  -p 53:53 -p 53:53/udp -d -i -t  digitalreachinsight/bind-ubuntu:$version
    fi
 fi
 if [ $unhealthycount -eq 0 ]
