c513b9000970c745bfd8c6ae7ab97f0e12cc9753
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2021-04-09 10:52:13 +0800

    Update postfix-ubuntu-docker.sh

--- postfix-ubuntu-docker.sh
+++ postfix-ubuntu-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version='v1.0.27'
+version='v1.0.28'
 imagename="postfix-ubuntu";
 unhealthycount=$(docker ps | grep unhealthy | grep $imagename | wc -l);
 healthy=$(docker ps | grep "(healthy)" | grep $imagename | grep $version | wc -l);
