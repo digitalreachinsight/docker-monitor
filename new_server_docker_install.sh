03f0af7a1983709c8ef299b5fc16635ed3f76b82
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2021-04-09 15:41:01 +0800

    Update new_server_docker_install.sh

--- new_server_docker_install.sh
+++ new_server_docker_install.sh
@@ -13,9 +13,11 @@ echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 # Docker Install
 echo "Installing Docker Now"
 apt update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg-agent
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
 apt install -y docker-ce
 systemctl status docker
+
