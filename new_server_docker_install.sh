4b77eddbd4a44a517434186eb4b850743c6ad0e0
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2021-04-09 15:08:23 +0800

    Adding Swap Creation

--- new_server_docker_install.sh
+++ new_server_docker_install.sh
@@ -1,3 +1,17 @@
+echo "Creating Swap"
+free -m
+swapon -s
+dd if=/dev/zero of=/swapfile count=1024 bs=1M
+ls / | grep swapfile
+chmod 600 /swapfile
+ls -lh /swapfile
+mkswap /swapfile
+swapon /swapfile
+free -m
+echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
+
+# Docker Install
+echo "Installing Docker Now"
 apt update
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
