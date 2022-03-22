255b67e6edb17db03d85e52573076e2b7af9f1b3
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2022-03-22 23:17:11 +0800

    Update php-apache2-ubuntu-docker.sh

--- php-apache2-ubuntu-docker.sh
+++ php-apache2-ubuntu-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #version='v1.0.12'
-version='2021.12.07.14.5029'
+version='2022.03.22.14.5139'
 imagename="phpapache2ubuntu";
 unhealthycount=$(docker ps | grep unhealthy | grep $imagename | wc -l);
 healthy=$(docker ps | grep "(healthy)" | grep $imagename | grep $version | wc -l);
