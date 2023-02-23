d2428c191470d9d362220c42ba7569f657e6cec4
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2023-02-23 21:11:41 +0800

    Create vscode-server-docker.sh

---
+++ vscode-server-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version='latest'
+imagename="vscode-server";
+unhealthycount=$(docker ps | grep unhealthy | grep $imagename | wc -l);
+healthy=$(docker ps | grep "(healthy)" | grep $imagename | grep $version | wc -l);
+starting=$(docker ps | grep starting | grep $imagename | wc -l);
+echo $healthy;
+echo $starting;
+if [ $healthy -eq 0 ]
+then
+   if [ $starting -eq 0 ]
+   then
+      echo "Unhealthy - restarting";
+      docker stop $imagename
+      docker rm $imagename
+      docker container run --hostname=$1 --name=$imagename -e PUID=33 -e PGID=33 -e PASSWORD=vscode2023! --mount type=bind,source=/container-data/,target=/container-data/  -p 8443:8443 -d -i -t  lscr.io/linuxserver/code-server:$version
+   fi
+fi
+if [ $unhealthycount -eq 0 ]
+then
+   echo "Healthy";
+fi
