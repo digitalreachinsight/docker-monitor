55b4ff8b50492ea6c3207957aa70af93c39b19fa
digitalreachinsight <43061336+digitalreachinsight@users.noreply.github.com>
2025-01-17 22:05:16 +0800

    Update nginx-ubuntu-docker.sh

--- nginx-ubuntu-docker.sh
+++ nginx-ubuntu-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #version='1.0.6'
 #version='2024.02.12.11.0629'
-version='2024.07.06.14.0621'
+#version='2024.07.06.14.0621'
+version='2025.01.17.13.3821'
 imagename="nginxserver"
 unhealthycount=$(docker ps | grep unhealthy | grep $imagename | wc -l);
 healthy=$(docker ps | grep "(healthy)" | grep $imagename | grep $version | wc -l);
